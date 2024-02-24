4f9d6774efaab90b2d024905b9c80d2788fbb905
thanhthao020303 <thanhthao020303@gmail.com>
2024-02-24 21:33:50 +0700

    2402024

---
+++ TT24042024/index.py
@@ -0,0 +1,27 @@
+import requests
+
+
+# URL của MeiliSearch API
+url = "http://127.0.0.1:7700/indexes"
+
+# Tên của index bạn muốn tạo
+index_name = "music"
+
+# Headers chứa MASTER_KEY
+headers = {
+    "Content-Type": "application/json",
+    "X-Meili-API-Key": "d55rxOn9BsUkOXE-VoALZi61NyEKbcEf61erMmNCINs"
+}
+# Dữ liệu JSON để tạo index
+data = {
+    "uid": index_name
+}
+
+# Gửi yêu cầu POST để tạo index
+response = requests.post(url, headers=headers, json=data)
+
+# Kiểm tra phản hồi từ MeiliSearch
+if response.status_code == 202:  # 202: Accepted
+    print(f"Index '{index_name}' đã được tạo thành công!")
+else:
+    print(f"Lỗi: {response.json()}")
